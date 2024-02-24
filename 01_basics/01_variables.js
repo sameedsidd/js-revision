ca882c9c5866462fc98af4a462e497b3e8822cd3
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-02-24 19:16:09 +0500

    revision

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,13 +1,13 @@
-const accountId = 1223344
-let accountEmail = "sameedsiddiqui9@gmail.com"
-var accountPassword = "12345"
-accountCity = "Karachi"
-let accountState;
+// const accountId = 1223344
+// // let accountEmail = "sameedsiddiqui9@gmail.com"
+// var accountPassword = "12345"
+// accountCity = "Karachi"
+// let accountState;
 
 // accountId = 2 // not allowed
-accountEmail = "b@gmail.com"
-accountPassword = "444"
-accountCity = "Lahore"
+// accountEmail = "b@gmail.com"
+// accountPassword = "444"
+// accountCity = "Lahore"
 
 /*
 prefer not to use var
@@ -15,5 +15,26 @@ because of issue in block or functional scope
 */
  
 // console.log(accountId); 
-console.table([accountEmail, accountPassword, accountPassword, accountCity, accountState]);
+// console.table([accountEmail, accountPassword, accountPassword, accountCity, accountState]);
+
+const accountId = 11223344
+let accountEmail = "sameedsiddiqui9@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+let accountState;
+
+
+
+// accountId = 3333 // not allowed
+accountEmail = "c@gmail.com"
+accountPassword = "54321"
+accountCity = "Lahore"
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountState, accountCity]);
+
+
+
+
 
