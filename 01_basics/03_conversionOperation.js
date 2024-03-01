3617960b7259801afbed1d0e9d447167ca38007b
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-03-01 19:44:22 +0500

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// any data type convert in number (Number)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// any data type convert in boolean (Boolean)
+
+// let isLoggedIn = 1
+// let isLoggedIn = "Hitesh"
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => fasle
+// "hitesh" => true
+
+// any data type convert in string (String)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
