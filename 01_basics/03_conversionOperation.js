0929b7ee1e0809bbbcab32d308c04dde85764469
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-03-01 22:50:18 +0500

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,8 +19,8 @@
 
 // let isLoggedIn = 1
 // let isLoggedIn = "Hitesh"
-let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
 
 // 1 => true, 0 => false
@@ -29,7 +29,67 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 // any data type convert in string (String)
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+let someNumber = 33;
+let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ********************* Operations *************************** //
+
+let value = 3;
+let negVal = -value;
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+
+let str1 = "Hello";
+let str2 = " Hitesh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + "2" + "2");
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+// console.log(+true);
+// console.log(true+);
+
+// console.log(+"");
+
+
+// let num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter);
+
+// postfix increment
+let x = 3
+let y = x++
+console.log(`x: ${x}, y: ${y}`);
+
+// prefix increment
+let a = 3
+let b = ++a
+console.log(`a: ${a}, b: ${b}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
